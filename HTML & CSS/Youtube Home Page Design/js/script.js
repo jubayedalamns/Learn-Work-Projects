0193807109311476ce5821dfbd4f6205182f86e1
jubayedalamns <jubayedalamns@gmail.com>
2023-02-19 11:18:36 -0800

    Main Container (Banner Image) Design Complite

---
+++ HTML & CSS/Youtube Home Page Design/js/script.js
@@ -0,0 +1,5 @@
+var MenuIcon = document.querySelector(".menu-icon");
+var sidebar = document.querySelector(".sidebar");
+MenuIcon.onclick=function(){
+    sidebar.classList.toggle("small-sidebar");
+}
